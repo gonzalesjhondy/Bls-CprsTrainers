93011b1882fb8b80af99600c98150a132c510aa2
unknown <gonzalesjhondy>
2024-05-07 00:06:21 +0800

    adding admin tempplate

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,54 @@
+  <!-- footer content -->
+  <footer>
+          <div class="pull-right">
+            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="{{ asset('vendor/jquery/dist/jquery.min.js') }} "></script>
+    <!-- Bootstrap -->
+    <script src="{{ asset('bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- FastClick -->
+    <script src="{{ asset('vendor/fastclick/lib/fastclick.js') }}"></script>
+    <!-- NProgress -->
+    <script src="{{ asset('vendor/nprogress/nprogress.js') }}"></script>
+    <!-- Chart.js -->
+    <script src="{{ asset('vendor/Chart.js/dist/Chart.min.js') }}"></script>
+    <!-- gauge.js -->
+    <script src="{{ asset('vendor/gauge.js/dist/gauge.min.js') }}"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="{{ asset('bootstrap/bootstrap-progressbar/bootstrap-progressbar.min.js') }}"></script>
+    <!-- iCheck -->
+    <script src="{{ asset('vendor/vendors/iCheck/icheck.min.js') }}"></script>
+    <!-- Skycons -->
+    <script src="{{ asset('vendor/vendors/skycons/skycons.js') }}"></script>
+    <!-- Flot -->
+    <script src="{{ asset('vendor/vendors/Flot/jquery.flot.js') }}"></script>
+    <script src="{{ asset('vendor/vendors/Flot/jquery.flot.pie.js') }}"></script>
+    <script src="{{ asset('vendor/vendors/Flot/jquery.flot.time.js') }}"></script>
+    <script src="{{ asset('vendor/vendors/Flot/jquery.flot.stack.js') }}"></script>
+    <script src="{{ asset('vendor/vendors/Flot/jquery.flot.resize.js') }}"></script>
+    <!-- Flot plugins -->
+    <script src="{{ asset('vendor/vendors/flot.orderbars/js/jquery.flot.orderBars.js') }}"></script>
+    <script src="{{ asset('vendor/vendors/flot-spline/js/jquery.flot.spline.min.js') }}"></script>
+    <script src="{{ asset('vendor/vendors/flot.curvedlines/curvedLines.js') }}"></script>
+    <!-- DateJS -->
+    <script src="{{ asset('vendor/vendors/DateJS/build/date.js') }}"></script>
+    <!-- JQVMap -->
+    <script src="{{ asset('vendor/vendors/jqvmap/dist/jquery.vmap.js') }}"></script>
+    <script src="{{ asset('vendor/vendors/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script>
+    <script src="{{asset('vendor/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js') }}"></script>
+    <!-- bootstrap-daterangepicker -->
+    <script src="{{ asset('vendor/vendors/moment/min/moment.min.js') }}"></script>
+    <script src="{{ asset('bootstrap/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="{{ asset('resources/js/custom.min.js') }}"></script>
+	
+  </body>
+</html>
